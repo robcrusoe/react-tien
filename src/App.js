8a38b1a6748161a24b59b5825bcad5fdc1e5603d
Arka Sain <arka.sain@aol.com>
2021-05-24 10:56:14 +0200

    Working with `useState()`

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [packageName, setPackageName] = useState('Not Delivered');
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h1>The package is: {packageName}</h1>
+      <button type='button' onClick={() => { setPackageName('Delivered') }}>Deliver</button>
     </div>
   );
 }
